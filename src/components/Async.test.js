041c9d546653d122df813acc60256999ec92cb50
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-21 13:33:55 +0200

    mocks

---
+++ src/components/Async.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe("Async component", () => {
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", tile: "post 1" }],
+    });
+    render(<Async />);
+    const listItemsElements = await screen.findAllByRole("listitem");
+    expect(listItemsElements).not.toHaveLength(0);
+  });
+});
